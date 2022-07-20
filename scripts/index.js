d227074694c1697e47bad8e9a4caa5a2f4d09abc
quicksilversel <quicksilversel@gmail.com>
2022-07-20 12:03:21 +0900

    add dark mode

--- scripts/index.js
+++ scripts/index.js
@@ -21,11 +21,61 @@ $(document).ready(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+    //identify the toggle switch HTML element
+    const toggleSwitch = document.querySelector('#theme-switch input[type="checkbox"]');
+
+    /**************** Dark Mode ****************/
+
+    // determines if the user has a set theme
+    function detectColorScheme(){
+
+        var theme="light"; //default to light
+
+        //local storage is used to override OS theme settings
+        if(localStorage.getItem("theme")){
+            if(localStorage.getItem("theme") == "dark"){
+                var theme = "dark";
+            }
+        } else if(!window.matchMedia) {
+            // matchMedia method not supported
+            return false;
+        } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            //OS theme setting detected as dark
+            var theme = "dark";
+        }
+
+        //dark theme preferred, set document with a `data-theme` attribute
+        if (theme=="dark") {
+            $('nav').removeClass('navbar-light');
+            $('nav').addClass('navbar-dark');
+            document.documentElement.setAttribute("data-theme", "dark");
+        }
+    }
+    detectColorScheme();
+
+    //function that changes the theme, and sets a localStorage variable to track the theme between page loads
+    function switchTheme(e) {
+        if (e.target.checked) {
+            localStorage.setItem('theme', 'dark');
+            $('nav').removeClass('navbar-light');
+            $('nav').addClass('navbar-dark');
+            document.documentElement.setAttribute('data-theme', 'dark');
+            toggleSwitch.checked = true;
+        } else {
+            localStorage.setItem('theme', 'light');
+            $('nav').removeClass('navbar-dark');
+            $('nav').addClass('navbar-light');
+            document.documentElement.setAttribute('data-theme', 'light');
+            toggleSwitch.checked = false;
+        }    
+    }
+
+    //listener for changing themes
+    toggleSwitch.addEventListener('change', switchTheme, false);
+
+    //pre-check the dark-theme checkbox if dark-theme is set
+    if (document.documentElement.getAttribute("data-theme") == "dark"){
+        toggleSwitch.checked = true;
+    }
 });
-
-/**************** Dark Mode Toggler ****************/
-
-function switchMode(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-} 
